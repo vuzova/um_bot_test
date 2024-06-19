9957597639d8a062e575dfe6513a5de17fc1cbcd
vasil <vasilina997@gmail.com>
2024-06-19 23:21:10 +0500

    Фикс обновление баллов

--- app/database/requests.py
+++ app/database/requests.py
@@ -21,10 +21,13 @@ async def get_user_scores(tg_id):
         user = await get_user(tg_id)
         return await session.scalars(select(Result).where(Result.user == user.id))
 
-async def get_user_scores_current_sbj(tg_id, sbj_id):
-    async with async_session() as session:
-        user = await get_user(tg_id)
-        return await session.scalar(select(Result).where(Result.user == user.id, Result.subject == sbj_id))
+async def get_user_scores_current_sbj(user_id, sbj_id, session=None):
+    if session is None:  # Если сессия не передана, используем новую
+        async with async_session() as session:
+            return await session.scalar(select(Result).where(Result.user == user_id, Result.subject == sbj_id))
+    else:  # Иначе используем переданную сессию
+        return await session.scalar(select(Result).where(Result.user == user_id, Result.subject == sbj_id))
+
 
 async def set_user(tg_id, name, surname):
     async with async_session() as session:
@@ -35,12 +38,13 @@ async def set_user(tg_id, name, surname):
 async def set_score(tg_id, subject_id, score):
     async with async_session() as session:
         user = await get_user(tg_id)
-        if not await get_user_scores_current_sbj(tg_id, subject_id):
+        if not await get_user_scores_current_sbj(user.id, subject_id):
             session.add(Result(user=user.id, subject=subject_id, score=score))
             await session.commit()
             return True
         else:
-            result = await get_user_scores_current_sbj(user.id, subject_id)
+            result = await get_user_scores_current_sbj(user.id, subject_id,  session=session)
             result.score = score
+            session.merge(result)
             await session.commit()
             return False
